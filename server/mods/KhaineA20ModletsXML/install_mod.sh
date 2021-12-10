e94ee88f74792d8e7b114c8e623109d26e59a273
tristelune <triste_lune_421@hotmail.com>
2021-12-10 03:52:47 +0100

    kahin xml mods first test

--- server/mods/KhaineA20ModletsXML/install_mod.sh
+++ server/mods/KhaineA20ModletsXML/install_mod.sh
@@ -1,6 +1,10 @@
-set ken_mod_familly_name=KhaineA20ModletsXML
+ken_mod_familly_name=KhaineA20ModletsXML
 git clone https://github.com/KhaineGB/$ken_mod_familly_name.git
-chmod 777 $ken_mod_familly_name
+chmod -r 777 $ken_mod_familly_name
+wget https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/main/server/mods/$ken_mod_familly_name/mod_list
 
-for modename in KHA20-12CraftQueue KHA20-3SlotForge KHA20-HPBars KHA20-60BBM KHA20-96BBM KHA20-KillTracking KHA20-LockableInvSlots  
-cp -r ~/mods/$ken_mod_familly_name/$modename ~/serverfiles/Mods/$modename
+inputfile="mod_list"
+while IFS=, read -r y
+do
+cp -r ~/mods/$ken_mod_familly_name/$y ~/serverfiles/Mods/$y
+done < $inputfile
