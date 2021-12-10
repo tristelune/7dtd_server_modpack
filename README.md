8947a6e1bf4394b804b7f17a66ad1df9d04de9d8
tristelune <triste_lune_421@hotmail.com>
2021-12-10 02:24:16 +0100

    update first test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 7dtd_server_modpack
- 
+
+# build for debian server
+
+install server mods : 
+wget https://github.com/tristelune/7dtd_server_modpack/blob/main/server/install_mods.sh
+sh install_mods.sh
--- server/install_mods.sh
+++ server/install_mods.sh
@@ -1,5 +1,26 @@
 cd ~
-mkdir -P ~/serverfiles/Mods
+if [ ! -d "~/serverfiles/Mods" ] 
+then
+	mkdir -P ~/serverfiles/Mods
+fi
+
+
 mkdir mods
 cd mods
-wget mod_list.txt
+wget https://github.com/tristelune/7dtd_server_modpack/blob/main/server/mod_list.txt
+
+inputfile="mods_list.txt"
+while IFS=, read -r y
+do
+mkdir $y
+chmod 777 $y
+cd $y
+wget https://github.com/tristelune/7dtd_server_modpack/blob/main/server/mods/$y/install_mod.sh
+sh ./install_mod.sh
+cd ..
+rmdir -rf $y
+done < $inputfile
+
+cd ..
+rmdir -rf mods
+
