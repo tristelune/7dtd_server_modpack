6d739730f705610cafa33f68c10964bc7bbeac6f
tristelune <triste_lune_421@hotmail.com>
2021-12-10 02:47:03 +0100

    Update install_mods.sh
    debug mkdir option
    
    debug raw ghitub url for file access

--- server/install_mods.sh
+++ server/install_mods.sh
@@ -1,13 +1,13 @@
 cd ~
 if [ ! -d "~/serverfiles/Mods" ] 
 then
-	mkdir -P ~/serverfiles/Mods
+	mkdir -p ~/serverfiles/Mods
 fi
 
 
 mkdir mods
 cd mods
-wget https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/blob/main/server/mods_list.txt
+wget https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/main/server/mods_list.txt
 
 inputfile="mods_list.txt"
 while IFS=, read -r y
@@ -15,7 +15,7 @@ do
 mkdir $y
 chmod 777 $y
 cd $y
-wget https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/blob/main/server/mods/$y/install_mod.sh
+wget https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/main/server/mods/$y/install_mod.sh
 sh ./install_mod.sh
 cd ..
 rmdir -rf $y
