02a0b86dea37f630a58da38efafaaad5ba022909
tristelune <triste_lune_421@hotmail.com>
2021-12-10 22:18:25 +0100

    Update install_mod.sh
    add GIT_SSL_NO_VERIFY=true

--- server/mods/KhaineA20ModletsXML/install_mod.sh
+++ server/mods/KhaineA20ModletsXML/install_mod.sh
@@ -1,4 +1,5 @@
 ken_mod_familly_name=KhaineA20ModletsXML
+GIT_SSL_NO_VERIFY=true
 git clone https://github.com/KhaineGB/$ken_mod_familly_name.git
 chmod -R 777 $ken_mod_familly_name
 wget --timestamping --no-check-certificate https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/main/server/mods/$ken_mod_familly_name/mod_list
