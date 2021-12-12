b8ae79245767de7e973df8857785ee2ba3e2e580
tristelune <triste_lune_421@hotmail.com>
2021-12-12 03:15:19 +0100

    corrected download & execut mods script

--- players/install_mods.ps1
+++ players/install_mods.ps1
@@ -10,17 +10,11 @@ cd $mod
 $url_name = "https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/main/players/mods/$mod"
 $file_name = "install_mod.ps1"
 Invoke-WebRequest -OutFile ./$file_name $url_name/$file_name
-invoke-expression -Command $file_name 
+invoke-expression -Command ./$file_name 
 cd ..
-
-
-
-
-
-
 }
- $path1=split-path -path $line -parent
- $path2=split-path -path $path1 -noqualifier
- $path=$files_path+$path2
- New-Item -ItemType directory -Path "$path" -Force
- copy-item -path "$line" -destination "$path" -Force
+ # $path1=split-path -path $line -parent
+ # $path2=split-path -path $path1 -noqualifier
+ # $path=$files_path+$path2
+ # New-Item -ItemType directory -Path "$path" -Force
+ # copy-item -path "$line" -destination "$path" -Force
