75d364c96012736952b534143094e3193d246a9d
tristelune <triste_lune_421@hotmail.com>
2021-12-12 23:19:20 +0100

    wrong argument use to read file settings and revert some previous change

--- players/install_mods.ps1
+++ players/install_mods.ps1
@@ -32,7 +32,7 @@ $file_name = $mods_list_file_name
 Invoke-WebRequest -OutFile ./$file_name $url_name/$file_name
 
 #read mods list file
-$file_read=get-content -Path $PSScriptRoot\$file_name | Where-Object { $_.Trim() -ne '' }
+$file_read=get-content -Path $PSScriptRoot\$mods_list_file_name | Where-Object { $_.Trim() -ne '' }
 
 #run throug all mods in the list
 foreach ($mod in $file_read) 
@@ -46,8 +46,8 @@ $file_name = $mod_install_script_name
 Invoke-WebRequest -OutFile ./$file_name $url_name/$file_name
 
 #run mod script and pass to it : game folder path, game mods folder name, current mod name, gith hub url for current mod
-$commande='./' + "$file_name" + ' ' + "$game_folder_path" + ' ' + "$game_mods_folder_name" + ' ' + "$mod" + ' ' + "$url_name" 
-invoke-expression -Command "$commande"
+$commande='./' + $file_name + ' ' + $game_folder_path + ' ' + $game_mods_folder_name + ' ' + $mod + ' ' + $url_name 
+invoke-expression -Command $commande
 
 #get out of current mod folder before next loop for next mod
 cd ..
