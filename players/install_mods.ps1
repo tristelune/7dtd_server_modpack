00bf0c0c82f1c88e04e8cbb83ee0f0e80a82be87
tristelune <triste_lune_421@hotmail.com>
2021-12-12 23:11:55 +0100

    test

--- players/install_mods.ps1
+++ players/install_mods.ps1
@@ -47,7 +47,7 @@ Invoke-WebRequest -OutFile ./$file_name $url_name/$file_name
 
 #run mod script and pass to it : game folder path, game mods folder name, current mod name, gith hub url for current mod
 $commande='./' + "$file_name" + ' ' + "$game_folder_path" + ' ' + "$game_mods_folder_name" + ' ' + "$mod" + ' ' + "$url_name" 
-invoke-expression -Command $commande
+invoke-expression -Command "$commande"
 
 #get out of current mod folder before next loop for next mod
 cd ..
