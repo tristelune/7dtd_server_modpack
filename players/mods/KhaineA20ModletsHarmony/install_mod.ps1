9d8aafed26c429d0d54dbc165d68b99122021b60
tristelune <triste_lune_421@hotmail.com>
2021-12-12 03:08:46 +0100

    Update install_mod.ps1

--- players/mods/KhaineA20ModletsHarmony/install_mod.ps1
+++ players/mods/KhaineA20ModletsHarmony/install_mod.ps1
@@ -1,5 +1,4 @@
-set mod_name=KhaineA20ModletsHarmony
-
+$mod_name="KhaineA20ModletsHarmony"
 
 $url_name = "https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/main/players/mods/$mod_name"
 $file_name = "mod_list.txt"
