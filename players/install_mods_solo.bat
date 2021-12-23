df645022363a8f0cd3de29315d568ce978663b85
tristelune <triste_lune_421@hotmail.com>
2021-12-23 22:35:25 +0100

    update
    add solo mods install
    pass mode list file name in parametters

---
+++ players/install_mods_solo.bat
@@ -0,0 +1,56 @@
+@echo off
+REM ###############################################
+REM # variables                                   #
+REM ###############################################
+
+REM set repo url
+set repo_url_name=https://raw.githubusercontent.com/tristelune/7dtd_server_modpack/main/players/
+
+REM set install script name
+set mods_install_script_name=install_mods.ps1
+
+REM set install script name
+set mod_folder_name=Mods
+
+REM set temp folder name
+set temp_mod_install_folder_name=temp_mods
+
+REM game file to check
+set game_file_test=7DaysToDie.exe
+
+REM mods list file name
+set mods_list_file_name=mods_list_solo.txt
+
+REM ###############################################
+REM # traitement                                  #
+REM ###############################################
+
+REM go to script folder
+CD /d "%~dp0"
+set current_folder="%CD%"
+REM check if in game folder if not exit
+IF NOT EXIST %game_file_test% (
+echo ***********************************************************************************
+echo must be run in 7dtd game root folder where the game executable 7DaysToDie.exe is
+echo ***********************************************************************************
+pause
+goto end
+)
+ 
+REM check if mod folder exist if not create it 
+IF NOT EXIST %mod_folder_name%/ md %mod_folder_name%
+
+REM create temp folder for installation
+md %temp_mod_install_folder_name%
+cd %temp_mod_install_folder_name%
+
+REM download powershell installation script
+set url_name=%repo_url_name%
+set file_name=%mods_install_script_name%
+powershell.exe -Command "Invoke-WebRequest -OutFile ./%file_name% %url_name%/%file_name%"
+Powershell -ExecutionPolicy bypass -file "%file_name%" "%current_folder%" "%mod_folder_name%" "%mods_list_file_name%"
+
+CD /d "%~dp0"
+IF EXIST %temp_mod_install_folder_name%/ rd /s /q %temp_mod_install_folder_name%
+
+:end
--- players/mods/KhaineA20ModletsXML/install_mod.ps1
+++ players/mods/KhaineA20ModletsXML/install_mod.ps1
@@ -1,1 +1,49 @@
-mod_name=KhaineA20ModletsXML
+###############################################
+# variables                                   #
+###############################################
+
+#set game mods folder
+$game_mods_folders_name=$args[1]
+
+#set game folder path
+$game_folder_path=$args[0]
+
+# set mode name
+$mod_name=$args[2]
+
+#set current mod url 
+$mod_script_url=$args[3]
+
+#set mod list file ghitub link 
+$mod_list_file_name=$args[3]
+
+#set link to mod files archive to download
+$mod_archive_url="https://github.com/KhaineGB/$mod_name/archive/refs/heads/"
+$mod_archive_file_name="main.zip"
+
+###############################################
+# traitement                                  #
+###############################################
+
+#download modlist
+$url_name = $mod_script_url
+$file_name = $mod_list_file_name
+Invoke-WebRequest -OutFile .\$file_name $url_name/$file_name
+
+#download mode archive
+$url_name = $mod_archive_url
+$file_name = $mod_archive_file_name
+Invoke-WebRequest -OutFile .\$file_name $url_name/$file_name
+
+#trim extention from archive name
+$out_path=$mod_archive_file_name.SubString(0,$mod_archive_file_name.Length-4)
+#unzip archive to archive name folder
+Expand-Archive -Path .\$file_name -DestinationPath .\$out_path -Force
+
+#read content of mod list 
+$file_read=get-content -Path $PSScriptRoot\$mod_list_file_name | Where-Object { $_.Trim() -ne '' }
+# copy each mod to mod folder
+foreach ($mod in $file_read) 
+{
+robocopy ".\$out_path\$mod_name-main\$mod"  "$game_folder_path\$game_mods_folders_name\$mod" /mir
+}
